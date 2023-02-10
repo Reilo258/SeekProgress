d2db15c64efb4422d8716d2001d35e0243ae8266
Jocheu <88211597+Jocheu@users.noreply.github.com>
2023-02-10 15:52:19 +0100

    Napisany kod do pierwszego seekbaru poziomego i pionowego

--- app/src/main/java/com/example/seekprogress/MainActivity.kt
+++ app/src/main/java/com/example/seekprogress/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.seekprogress
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //images
+        val img1 = findViewById<ImageView>(R.id.imageView)
+        val img2 = findViewById<ImageView>(R.id.imageView2)
+        val img3 = findViewById<ImageView>(R.id.imageView3)
+
+        //widthbars
+        val wb1 = findViewById<SeekBar>(R.id.seekBar)
+        val wb2 = findViewById<SeekBar>(R.id.seekBar2)
+        val wb3 = findViewById<SeekBar>(R.id.seekBar3)
+        wb1.progress = 100;
+        wb2.progress = 100;
+        wb3.progress = 100;
+
+        //heightbars
+        val hb1 = findViewById<SeekBar>(R.id.seekBar4)
+        val hb2 = findViewById<SeekBar>(R.id.seekBar5)
+        val hb3 = findViewById<SeekBar>(R.id.seekBar6)
+        hb1.progress = 100;
+        hb2.progress = 100;
+        hb3.progress = 100;
+
+
+
+        wb1.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                img1.scaleX = progress.toFloat() / 100f
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        hb1.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                img1.scaleY = progress.toFloat() / 100f
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
     }
 }
